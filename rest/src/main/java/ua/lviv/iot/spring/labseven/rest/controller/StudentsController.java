0cae22782fb3f0f9577bf571fe32d15869820be4
Denys-Doskochynskiy <doskochynska83@gmail.com>
2020-03-20 10:09:55 +0200

    Fix code and added methods for updating and deleting data

--- rest/src/main/java/ua/lviv/iot/spring/labseven/rest/controller/StudentsController.java
+++ rest/src/main/java/ua/lviv/iot/spring/labseven/rest/controller/StudentsController.java
@@ -1,9 +1,21 @@
 package ua.lviv.iot.spring.labseven.rest.controller;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.lviv.iot.spring.labseven.rest.model.Student;
@@ -11,16 +23,37 @@ import ua.lviv.iot.spring.labseven.rest.model.Student;
 @RequestMapping("/students")
 @RestController
 public class StudentsController {
+static	private Map<Integer, Student> students = new HashMap<>();
+static	private AtomicInteger idCounter = new AtomicInteger();
 
-  @GetMapping(path = "/{id}")
-  public Student getStudent(@PathVariable("id") Integer studentId) {
-    System.out.println(studentId);
-    return new Student("Maria", "vasa");
+	@GetMapping
+	public List<Student> getStudents() {
+		return new LinkedList<Student>(students.values());
+	}
+
+	@GetMapping(path = "/{id}")
+	public Student getStudent(final @PathVariable("id") Integer studentId) {
+		return students.get(studentId);
+	}
+
+	@PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+
+  public Student createStudent(final @RequestBody Student student) {
+	  student.setId(idCounter.incrementAndGet());
+	  students.put(student.getId(), student);
+	  return student ;
   }
 
-  @RequestMapping(method = { RequestMethod.GET, RequestMethod.PUT }, path = "/smartest")
-  public Student getSmartestStudents() {
-    return null;
-  }
-
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Student> deleteStudent(@PathVariable("id") Integer studentId) {
+HttpStatus status = students.remove(studentId)==null ?HttpStatus.NOT_FOUND :HttpStatus.OK;
+		
+		return ResponseEntity.status(status).build();
+	}
+	@PutMapping(path="/{id}")
+	public Student updateStudent(final @PathVariable("id") Integer studentId,final @RequestBody Student student) {
+		student.setId(studentId);
+	
+		return students.put(studentId,student);
+	}
 }
--- rest/src/main/java/ua/lviv/iot/spring/labseven/rest/model/Student.java
+++ rest/src/main/java/ua/lviv/iot/spring/labseven/rest/model/Student.java
@@ -1,24 +1,47 @@
 package ua.lviv.iot.spring.labseven.rest.model;
 
-public class Student {
-private String firsStudent;
-private String secondStudent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 
-public Student(String firstStudent,String secondStudent) {
-	super();
-	this.firsStudent =firstStudent;
-	this.secondStudent= secondStudent;
-}
-public String getSecondStudent() {
-	return secondStudent;
-}
-public void setSecondStudent(String secondStudent) {
-	this.secondStudent = secondStudent;
-}
-public String getFirsStudent() {
-	return firsStudent;
-}
-public void setFirsStudent(String firsStudent) {
-	this.firsStudent = firsStudent;
-}
+public class Student {
+	private String firsStudent;
+	private String secondStudent;
+	private Integer id;
+
+	public Student(String firstStudent, String secondStudent) {
+		super();
+		// this.id=id;
+		this.firsStudent = firstStudent;
+		this.secondStudent = secondStudent;
+	}
+
+	public Student() {
+	}
+
+	public String getSecondStudent() {
+		return secondStudent;
+	}
+
+	public void setSecondStudent(String secondStudent) {
+		this.secondStudent = secondStudent;
+	}
+
+	public String getFirsStudent() {
+		return firsStudent;
+	}
+
+	public void setFirsStudent(String firsStudent) {
+		this.firsStudent = firsStudent;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
 }
